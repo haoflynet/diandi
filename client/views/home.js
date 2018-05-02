9e11a5a3626970e2985f478466bb5700411d16f6
haoflynet <haoflynet@gmail.com>
2018-05-02 22:14:15 +0800

    add HomeScreen

---
+++ client/views/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+export class HomeScreen extends Component {
+    render() {
+        return (
+          <View>
+            <Text>abc</Text>
+          </View>
+        )
+      }
+}
