a9e831313c58d71f80c1f7df0d86d376e0b765e0
haoflynet <haoflynet@gmail.com>
2018-05-03 22:02:16 +0800

    add buttons and functions

--- client/views/home.js
+++ client/views/home.js
@@ -1,13 +1,67 @@
+/**
+ * 首页
+ */
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 
 export class HomeScreen extends Component {
-    render() {
-        return (
-          <View>
-            <Text>abc</Text>
-          </View>
-        )
-      }
+  /**
+   * 录音按钮
+   */
+  _recordSound () {
+    console.log('_recordSound');
+  }
+  /**
+   * 查询记录
+   */
+  _searchRecord () {
+    console.log('_searchRecord');
+  }
+  /**
+   * 日常记录
+   */
+  _record () {
+    console.log('_record');
+  }
+  /**
+   * 普通提醒
+   */
+  _remind () {
+    console.log('_remind');
+  }
+  /**
+   * 模糊提醒
+   */
+  _fuzzyRemind () {
+    console.log('_fuzzyRemind');
+  }
+
+  render() {
+      return (
+        <View>
+          <Text>HomeScreen</Text>
+          <Button
+            onPress={ () => this._recordSound() }
+            title="录音按钮"
+          />
+          <Button
+            onPress={ () => this._searchRecord() }
+            title="查询记录"
+          />
+          <Button
+            onPress={ () => this._record() }
+            title="日常记录"
+          />
+          <Button
+            onPress={ () => this._remind() }
+            title="普通提醒"
+          />
+          <Button
+            onPress={ () => this._fuzzyRemind() }
+            title="模糊提醒"
+          />
+        </View>
+      )
+    }
 }
