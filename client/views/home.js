b6912ff066f34fb77b71b10ea23b9aa6a7d8a10f
haoflynet <haoflynet@gmail.com>
2018-05-15 21:53:58 +0800

    add recognizing function and add voice logical code

--- client/views/home.js
+++ client/views/home.js
@@ -4,85 +4,58 @@
 import React, { Component } from 'react';
 import Voice from 'react-native-voice';
 import { View, Text, Button, Image, StyleSheet, TouchableHighlight} from 'react-native';
+import axios from 'axios';
+import { API_VOICES } from '../utils/constants/config';
 
 
 export class HomeScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      recognized: '',
-      pitch: '',
-      error: '',
-      end: '',
-      started: '',
-      results: [],
-      partialResults: [],
-    };
-    Voice.onSpeechStart = this.onSpeechStart.bind(this);
-    Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this);
-    Voice.onSpeechEnd = this.onSpeechEnd.bind(this);
-    Voice.onSpeechError = this.onSpeechError.bind(this);
-    Voice.onSpeechResults = this.onSpeechResults.bind(this);
-    Voice.onSpeechPartialResults = this.onSpeechPartialResults.bind(this);
-    Voice.onSpeechVolumeChanged = this.onSpeechVolumeChanged.bind(this);
+      voices: [],
+      text: '',
+    }
+
+    Voice.onSpeechPartialResults = this._onSpeechPartialResults.bind(this);
+    Voice.onSpeechResults = this._onSpeechResults.bind(this);
   }
 
-  componentWillUnmount() {
-    Voice.destroy().then(Voice.removeAllListeners);
-  }
-
-  onSpeechStart(e) {
-    this.setState({
-      started: '√',
+  componentDidMount() {
+    axios.get(API_VOICES).then((response) => {
+      voices = response.data.data;
+      this.setState({
+        voices: voices,
+      });
+      console.log(voices);
+    }).catch((error) => {
+      console.log('error in home.componentDidMount');
     });
   }
 
-  onSpeechRecognized(e) {
+  _onSpeechPartialResults(e) {
+    console.log('onSpeechPartialResults');
     this.setState({
-      recognized: '√',
+      text: e.value[0],
     });
+    console.log(e.value);
   }
 
-  onSpeechEnd(e) {
-    this.setState({
-      end: '√',
-    });
+  _onSpeechResults(e) {
+    console.log('onSpeechResults');
+    console.log(e.value);
   }
 
-  onSpeechError(e) {
-    this.setState({
-      error: JSON.stringify(e.error),
-    });
-  }
-
-  onSpeechResults(e) {
-    this.setState({
-      results: e.value,
-    });
-  }
-
-  onSpeechPartialResults(e) {
-    this.setState({
-      partialResults: e.value,
-    });
-  }
-
-  onSpeechVolumeChanged(e) {
-    this.setState({
-      pitch: e.value,
-    });
-  }
-
+  /**
+   * 按钮按下开始录音
+   * @param {*} e 
+   */
   async _startRecognizing(e) {
-    this.setState({
-      recognized: '',
-      pitch: '',
-      error: '',
-      started: '',
-      results: [],
-      partialResults: [],
-      end: ''
-    });
+    console.log('开始录音');
+
+    if (!Voice.isAvailable()	) {
+      console.error('没有录音权限');
+    }
+
     try {
       await Voice.start('zh-Hans');
     } catch (e) {
@@ -90,7 +63,12 @@ export class HomeScreen extends Component {
     }
   }
 
+  /**
+   * 按钮松开结束录音
+   * @param {*} e 
+   */
   async _stopRecognizing(e) {
+    console.log('结束录音');
     try {
       await Voice.stop();
     } catch (e) {
@@ -98,145 +76,44 @@ export class HomeScreen extends Component {
     }
   }
 
-  async _cancelRecognizing(e) {
-    try {
-      await Voice.cancel();
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  async _destroyRecognizer(e) {
-    try {
-      await Voice.destroy();
-    } catch (e) {
-      console.error(e);
-    }
-    this.setState({
-      recognized: '',
-      pitch: '',
-      error: '',
-      started: '',
-      results: [],
-      partialResults: [],
-      end: ''
+  /**
+   * 添加记录
+   */
+  async _addRecord() {
+    console.log('添加记录');
+    axios.post(API_VOICES, {
+      'text': this.state.text,
+    }).then((response) => {
+      console.log(response.data);
+    }).catch((error) => {
+      console.log(error);
     });
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native Voice!
-        </Text>
-        <Text style={styles.instructions}>
-          Press the button and start speaking.
-        </Text>
-        <Text
-          style={styles.stat}>
-          {`Started: ${this.state.started}`}
-        </Text>
-        <Text
-          style={styles.stat}>
-          {`Recognized: ${this.state.recognized}`}
-        </Text>
-        <Text
-          style={styles.stat}>
-          {`Pitch: ${this.state.pitch}`}
-        </Text>
-        <Text
-          style={styles.stat}>
-          {`Error: ${this.state.error}`}
-        </Text>
-        <Text
-          style={styles.stat}>
-          Results
-        </Text>
-        {this.state.results.map((result, index) => {
+      <View>
+        <TouchableHighlight onPressIn={this._startRecognizing.bind(this)} onPressOut={this._stopRecognizing.bind(this)}>
+          <View>
+            <Text>开始录音</Text>
+	        </View>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPressIn={this._addRecord.bind(this)}>
+          <View>
+            <Text>添加记录</Text>
+	        </View>
+        </TouchableHighlight>
+        
+        {this.state.voices.map((voice, index) => {
           return (
             <Text
-              key={`result-${index}`}
-              style={styles.stat}>
-              {result}
+              key={`voice-${voice.id}`}>
+              {voice.text}
             </Text>
           )
-        })}
-        <Text
-          style={styles.stat}>
-          Partial Results
-        </Text>
-        {this.state.partialResults.map((result, index) => {
-          return (
-            <Text
-              key={`partial-result-${index}`}
-              style={styles.stat}>
-              {result}
-            </Text>
-          )
-        })}
-        <Text
-          style={styles.stat}>
-          {`End: ${this.state.end}`}
-        </Text>
-        <TouchableHighlight onPress={this._startRecognizing.bind(this)}>
-          <Image
-            style={styles.button}
-            source={require('./button.png')}
-          />
-        </TouchableHighlight>
-        <TouchableHighlight onPress={this._stopRecognizing.bind(this)}>
-          <Text
-            style={styles.action}>
-            Stop Recognizing
-          </Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={this._cancelRecognizing.bind(this)}>
-          <Text
-            style={styles.action}>
-            Cancel
-          </Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={this._destroyRecognizer.bind(this)}>
-          <Text
-            style={styles.action}>
-            Destroy
-          </Text>
-        </TouchableHighlight>
+        })}      
       </View>
     );
-  }
-}
-
-const styles = StyleSheet.create({
-  button: {
-    width: 50,
-    height: 50,
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  action: {
-    textAlign: 'center',
-    color: '#0000FF',
-    marginVertical: 5,
-    fontWeight: 'bold',
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  stat: {
-    textAlign: 'center',
-    color: '#B0171F',
-    marginBottom: 1,
-  },
-});
+  };
+};
