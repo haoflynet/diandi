414b0063b5f62a7fd27f3a10ced069f523c2aa86
haoflynet <haoflynet@gmail.com>
2018-05-15 21:51:09 +0800

    add api file and api config file

---
+++ client/utils/constants/config.js
@@ -0,0 +1,4 @@
+export const API_DOMAIN = 'http://127.0.0.1:8888'
+export const API_VOICES = API_DOMAIN + '/voices/'
+export const API_ALARMS = API_DOMAIN + '/alarms/'
+export const API_RECORDS = API_DOMAIN + '/records/'
