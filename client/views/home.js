45eb086b763102854327fc415308873d256c0013
haoflynet <haoflynet@gmail.com>
2018-05-27 21:43:04 +0800

    add home image

--- client/views/home.js
+++ client/views/home.js
@@ -3,18 +3,65 @@
  */
 import React, { Component } from 'react';
 import Voice from 'react-native-voice';
-import { View, Text, Button, Image, StyleSheet, TouchableHighlight} from 'react-native';
+import { View, Text, Button, Image, StyleSheet, TouchableHighlight, TextInput, RefreshControl} from 'react-native';
 import axios from 'axios';
 import { API_VOICES, VOICE_TYPE_RECORD, VOICE_TYPE_ALARM } from '../utils/constants/config';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Timeline from 'react-native-timeline-listview';
 
 
 export class HomeScreen extends Component {
+  static navigationOptions = {
+    title: '点点记录',
+    headerLeft: (
+      <Button 
+        onPress={() => alert('个人中心暂未开放')}
+        title="个人"
+      />
+    ),
+    headerRight: (
+      <Button
+        onPress={() => alert('日历系统暂未开放')}
+        title="日历"
+      />
+    ),
+  };
+
   constructor(props) {
     super(props);
     this.state = {
       voices: [],
-      text: '',
+      text: '点击可直接输入',
     }
+
+    this.data = [
+      {
+        title: '我要成为一个很厉害的人', 
+        lineColor:'#009688', 
+        icon: require('../img/archery.png'),
+        imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240340/c0f96b3a-0fe3-11e7-8964-fe66e4d9be7a.jpg'
+      },
+      {
+        title: '下个月一定要完成这个项目', 
+        icon: require('../img/badminton.png'),
+        imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240405/0ba41234-0fe4-11e7-919b-c3f88ced349c.jpg'
+      },
+      {
+        title: '明天八点起床', 
+        icon: require('../img/lunch.png'),
+      },
+      {
+        title: 'Watch Soccer', 
+        lineColor:'#009688', 
+        icon: require('../img/soccer.png'),
+        imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240419/1f553dee-0fe4-11e7-8638-6025682232b1.jpg'
+      },
+      {
+        title: 'Go to Fitness center', 
+        icon: require('../img/dumbbell.png'),
+        imageUrl: 'https://cloud.githubusercontent.com/assets/21040043/24240422/20d84f6c-0fe4-11e7-8f1d-9dbc594d0cfa.jpg'
+      }
+    ]
 
     Voice.onSpeechPartialResults = this._onSpeechPartialResults.bind(this);
     Voice.onSpeechResults = this._onSpeechResults.bind(this);
