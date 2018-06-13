d89630bd1ac611f1dd80f4534732ae72bcd4bcd6
haoflynet <haoflynet@gmail.com>
2018-06-13 22:35:02 +0800

    use agenda instead calendarlist

--- client/views/calendar.js
+++ client/views/calendar.js
@@ -2,7 +2,7 @@
  * 设置
  */
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button,StyleSheet } from 'react-native';
 import { Calendar, CalendarList, Agenda, Arrow } from 'react-native-calendars';
 import { Avatar, ListItem, Icon } from 'react-native-elements';
 import XDate from 'xdate';
@@ -19,87 +19,152 @@ export class CalendarScreen extends Component {
     
     this.state = {
       currentDate: new XDate(),
-      markedDates:  {}
+      markedDates:  {},
+      items: {}
     };
     this.state.markedDates[this.state.currentDate.toString('yyyy-MM-dd')] = {selected: true, marked: true, selectedColor: '#00adf5'}
   };
 
+  renderItem(item) {
+    return (
+      <View style={[styles.item, {height: item.height}]}><Text>{item.name}</Text></View>
+    );
+  }
+
+  renderEmptyDate() {
+    return null;
+    return (
+      <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
+    );
+  }
+
+  rowHasChanged(r1, r2) {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+  }
+
+  loadItems(day) {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+        if (!this.state.items[strTime]) {
+          this.state.items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 5);
+          for (let j = 0; j < numItems; j++) {
+            this.state.items[strTime].push({
+              name: 'Item for ' + strTime,
+              height: Math.max(50, Math.floor(Math.random() * 150))
+            });
+          }
+        }
+      }
+      //console.log(this.state.items);
+      const newItems = {};
+      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
+      this.setState({
+        items: newItems
+      });
+    }, 1000);
+    // console.log(`Load Items for ${day.year}-${day.month}`);
+  }
+
   render() {
     return (
-      <View>
-        <CalendarList
-          onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
-          pastScrollRange={50}
-          futureScrollRange={50}
-          scrollEnabled={true}
-          showScrollIndicator={true}
+      <Agenda
+        monthFormat={'yyyy年MM月'}
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems.bind(this)}
+        selected={'2017-05-16'}
+        renderItem={this.renderItem.bind(this)}
+        renderEmptyDate={this.renderEmptyDate.bind(this)}
+        rowHasChanged={this.rowHasChanged.bind(this)}
+        renderDay={(date, item) => {
 
-          minDate={'2018-01-01'}
-          onDayPress={(day) => {console.log('selected day', day)}}
-          monthFormat={'yyyy年MM月'}
+          function sameDate(a, b) {
+            return a instanceof XDate && b instanceof XDate &&
+              a.getFullYear() === b.getFullYear() &&
+              a.getMonth() === b.getMonth() &&
+              a.getDate() === b.getDate();
+          }
 
-          markedDates = {this.state.markedDates}
-        />
-        <Agenda
-          // the list of items that have to be displayed in agenda. If you want to render item as empty date
-          // the value of date key kas to be an empty array []. If there exists no value for date key it is
-          // considered that the date in question is not yet loaded
-          items={
-            {'2018-06-02': [{text: 'item 1 - any js object'}],
-            '2012-05-23': [{text: 'item 2 - any js object'}],
-            '2012-05-24': [],
-            '2012-05-25': [{text: 'item 3 - any js object'},{text: 'any js object'}],
-            }}
-          // callback that gets called when items for a certain month should be loaded (month became visible)
-          loadItemsForMonth={(month) => {console.log('trigger items loading')}}
-          // callback that fires when the calendar is opened or closed
-          onCalendarToggled={(calendarOpened) => {console.log(calendarOpened)}}
-          // callback that gets called on day press
-          onDayPress={(day)=>{console.log('day pressed')}}
-          // callback that gets called when day changes while scrolling agenda list
-          onDayChange={(day)=>{console.log('day changed')}}
-          // initially selected day
-          selected={'2012-05-16'}
-          // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-          minDate={'2012-05-10'}
-          // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-          maxDate={'2012-05-30'}
-          // Max amount of months allowed to scroll to the past. Default = 50
-          pastScrollRange={50}
-          // Max amount of months allowed to scroll to the future. Default = 50
-          futureScrollRange={50}
-          // specify how each item should be rendered in agenda
-          renderItem={(item, firstItemInDay) => {return (<View />);}}
-          // specify how each date should be rendered. day can be undefined if the item is not first in that day.
-          renderDay={(day, item) => {return (<View />);}}
-          // specify how empty date content with no items should be rendered
-          renderEmptyDate={() => {return (<View />);}}
-          // specify how agenda knob should look like
-          renderKnob={() => {return (<View />);}}
-          // specify what should be rendered instead of ActivityIndicator
-          renderEmptyData = {() => {return (<View />);}}
-          // specify your item comparison function for increased performance
-          rowHasChanged={(r1, r2) => {return r1.text !== r2.text}}
-          // Hide knob button. Default = false
-          hideKnob={true}
-          // By default, agenda dates are marked if they have at least one item, but you can override this if needed
-          markedDates={{
-            '2012-05-16': {selected: true, marked: true},
-            '2012-05-17': {marked: true},
-            '2012-05-18': {disabled: true}
-          }}
-          // agenda theme
-          // theme={{
-          //   ...calendarTheme,
-          //   agendaDayTextColor: 'yellow',
-          //   agendaDayNumColor: 'green',
-          //   agendaTodayColor: 'red',
-          //   agendaKnobColor: 'blue'
-          // }}
-          // agenda container style
-          style={{}}
-        />    
-      </View>
+          const today = sameDate(date, XDate()) ? styles.today : undefined;
+          console.log(date, item);
+          if (date) {
+            if (item) {
+              return (
+                <View style={styles.day}>
+                  <Text allowFontScaling={false} style={[styles.dayNum, today]}>{date.day}</Text>
+                  <Text allowFontScaling={false} style={[styles.dayText, today]}>{XDate.locales[XDate.defaultLocale].dayNamesShort[date.day]}</Text>
+                </View>
+              );
+            }
+
+          } else {
+            return (
+              <View style={styles.day}/>
+            );
+          }
+        }}
+
+        // markingType={'period'}
+        // markedDates={{
+        //    '2017-05-08': {textColor: '#666'},
+        //    '2017-05-09': {textColor: '#666'},
+        //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
+        //    '2017-05-21': {startingDay: true, color: 'blue'},
+        //    '2017-05-22': {endingDay: true, color: 'gray'},
+        //    '2017-05-24': {startingDay: true, color: 'gray'},
+        //    '2017-05-25': {color: 'gray'},
+        //    '2017-05-26': {endingDay: true, color: 'gray'}}}
+         // monthFormat={'yyyy'}
+         // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+        //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+      />
+
+      // </View>
     )
   }
 }
+
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
+  },
+  emptyDate: {
+    height: 15,
+    flex:1,
+    paddingTop: 30
+  },
+  day: {
+    width: 63,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginTop: 32
+  },
+  dayNum: {
+    fontSize: 28,
+    fontWeight: '200',
+    color: "#7a92a5"
+  },
+  dayText: {
+    fontSize: 14,
+    fontWeight: '300',
+    color: "#7a92a5",
+    marginTop: -5,
+    backgroundColor: 'rgba(0,0,0,0)'
+  },
+  today: {
+    color: '#00adf5',
+  },
+});
